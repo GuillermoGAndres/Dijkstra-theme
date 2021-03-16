8fa39c871e3084311424d3d621167cd5cbb12a26
rootGuillermo <guillermo@localhost.localdomain>
2021-03-15 22:11:42 -0600

    Add agregando mas colores para nuevos modos

--- dijkstra-theme.el
+++ dijkstra-theme.el
@@ -49,35 +49,35 @@
    ;; ----------------- Frame stuff --------------------
    `(default ((t (:background ,dijkstra-background :foreground ,dijkstra-foreground :weight semibold))))
    ;;`(default ((t (:background "#0d101e" :foreground "#d6d6d6" :weight semibold)))) ;; azul 1
-   ;;'(default ((t (:background "#121930" :weight semibold)))) ;; azul 2 
+   ;;'(default ((t (:background "#121930" :weight semibold)))) ;; azul 2
    ;;'(default ((t (:background "#13182a" :weight semibold)))) ;; azul 3
    ;;'(default ((t (:background "#0f1928" :weight semibold)))) ;; azul 4
    ;;'(default ((t (:background "#111928" :weight semibold)))) ;; azul 5
    ;;`(default ((t (:background "#0b0e18" :weight semibold)))) ;; azul 6
    ;;`(default ((t (:background "#111928" :weight semibold)))) ;; azul 7
-   ;;'(default ((t (:background "#222b43" :weight semibold)))) ;; azul muy luminoso   
+   ;;'(default ((t (:background "#222b43" :weight semibold)))) ;; azul muy luminoso
 
    `(cursor  ((t (:background ,dijkstra-off-white))))
    ;;Marcado de la linea actual si es que esta activado.
    `(hl-line ((t (:background ,dijkstra-mid-gray))))
-   
+
    ;; --------------------Mode line --------------------
    ;;`(mode-line ((t (:box nil :foreground ,dijkstra-foreground :background ,dijkstra-dark-blue))))
    `(mode-line ((t (:box nil :background ,dijkstra-mode-line-blue :foreground ,dijkstra-foreground))))
    ;;'(mode-line ((t (:background "#1e2338" :foreground :box nil))))
-   
+
    ;;`(mode-line-inactive ((t (:box nil :background ,dijkstra-mid-gray :foreground ,dijkstra-light-gray-blue))))
    `(mode-line-inactive ((t (:box nil :background ,dijkstra-modeI-line-blue :foreground ,dijkstra-light-gray-blue))))
    ;;`(mode-line-inactive ((t (:box nil :background "#0b0e18" ))))
-      
-   
+
+
    ;; Dir-ed search prompt
    ;;`(minibuffer-prompt ((default (:foreground ,dijkstra-white))))
    ;;`(minibuffer-prompt ((default (:foreground "#FFC8AE")))) ;;  #FFBA99 or #FFC8AE-naranja
    `(minibuffer-prompt ((default (:foreground "#F0DFAF")))) ;;  Amarillo
 
    ;; Highlight region color
-   ;`(region ((t (:foreground ,dijkstra-yellow-white :background ,dijkstra-darker-blue))))
+                                        ;`(region ((t (:foreground ,dijkstra-yellow-white :background ,dijkstra-darker-blue))))
    `(fringe ((t (:background ,dijkstra-background))))
 
    ;; ---------------- Code Highlighting ---------------
@@ -88,23 +88,23 @@
    ;; Comments
    '(font-lock-comment-face ((t (:foreground "#86B187" :slant italic)))) ; Comentarios de en linea
    '(font-lock-doc-face ((t (:foreground "#86B187")))) ;Docsting gris chido #9FB3C2 or verde chido #86B187 o verde con un tono poco nitido #8AB8A2.
-   
-   ;; Function names   
+
+   ;; Function names
    ;;'(font-lock-function-name-face ((t (:foreground "#F28B86")))) ; color salmon para las funcines
    '(font-lock-function-name-face ((t (:foreground "#F0DFAF")))) ; color amarillo fuerte igual al color que los types.
-   ;;'(font-lock-function-name-face ((t (:foreground "#FAFFDB")))) ; color amarillo claro  
+   ;;'(font-lock-function-name-face ((t (:foreground "#FAFFDB")))) ; color amarillo claro
    ;;'(font-lock-function-name-face ((t (:foreground "#FFDAB5")))) ; color naranja claro
 
    ;; Keywords
    '(font-lock-keyword-face ((t (:foreground "#8AC6F2" :weight bold)))) ;Color azul muy chido.
-   
+
    ;; Strings
    '(font-lock-string-face ((t (:foreground "#D1907F")))) ;Cadenas naranjas
-   
+
    ;; Variables
    '(font-lock-type-face ((t (:foreground "#F0DFAF")))) ; Clases y types amarillas
    '(font-lock-constant-face ((t (:foreground "#F0DFAF")))) ;Contastantes amarillas
-   
+
    ;;'(font-lock-variable-name-face ((t (:foreground "#9FB3C2")))) ;Color verde chido #8AB8A2 or gris #9FB3C2, para los nombre de los tipos.
    ;;'(font-lock-variable-name-face ((t (:foreground "#FFC8AE")))) ;#FFC8AE naranjas
    '(font-lock-variable-name-face ((t (:foreground "#d6d6d6")))) ; color de la letra
@@ -121,7 +121,7 @@
    '(company-scrollbar-bg ((t (:background "#282A2E")))) ;Color de scrollbar
    '(company-scrollbar-fg ((t (:background "#373B41"))))
    '(company-tooltip-annotation ((t (:foreground "#F0DFAF"))))
-   
+
    ;; ----------------------------Helm-----------------------------------------
    '(helm-source-header ((t (:extend t :foreground "#F0DFAF" :weight bold))))
    '(helm-match ((t (:extend t :foreground "#F0DFAF"))))
@@ -129,12 +129,12 @@
 
    '(helm-ff-directory ((t (:extend t :foreground "#F0DFAF"))))
    '(helm-ff-dotted-directory ((t (:extend t :foreground "#d6d6d6"))))
-   
+
    ;; --------------------------Tabs-------------------------------------
-   ; Buen color naranja: #e9967a
-   '(tab-bar ((t (:inherit variable-pitch :background "#0C1A29" :foreground "#F0DFAF")))) 
+                                        ; Buen color naranja: #e9967a
+   '(tab-bar ((t (:inherit variable-pitch :background "#0C1A29" :foreground "#F0DFAF"))))
    '(tab-bar-tab-inactive ((t (:inherit tab-bar-tab :background "#0C1A29" :foreground "#d6d6d6"))))
-   
+
    ;; -------------------------Highlight region color-----------------------------------
 
    ;; --------------------linum-mode --------------------------------
@@ -156,41 +156,56 @@
    '(treemacs-fringe-indicator-face ((t (:foreground "#86B187"))))
    ;;'(region ((t (:foreground ,hoare-light-yellow :background "gray37"))))
    '(region ((t (:background "gray37"))))
-   
+
    ;; ---------------- Package Specific Stuff -----------
    ;; Powerlinec
    `(powerline-active1 ((t (:background ,dijkstra-off-white :foreground ,dijkstra-background))))
 
    ;;--------------------Web mode --------------------
    '(web-mode-doctype-face ((t (:foreground "lightblue")))) ;turquoise , lightblue, PaleTurquoise
-   '(web-mode-html-tag-face ((t (:foreground "#FFF8DC")))); amarillo
-   ;;'(web-mode-html-tag-face ((t (:foreground "#8AC6F2")))); azul
+   ;;'(web-mode-html-tag-face ((t (:foreground "#FFF8DC")))); amarillo
+   '(web-mode-html-tag-face ((t (:foreground "#8AC6F2")))); azul
 
    ;;'(web-mode-html-tag-face ((t (:foreground "#EFFBFF"))));Color de los brackets
 
-   '(web-mode-html-attr-name-face ((t (:foreground "#FFDAB5")))) ; naranja claro
+   '(web-mode-html-attr-name-face ((t (:foreground "#fff4cc")))) ; amarillo muy claro
+   ;;'(web-mode-html-attr-name-face ((t (:foreground "#FFDAB5")))) ; naranja muy claro
+   ;;'(web-mode-html-attr-name-face ((t (:foreground "#FFF8DC")))) ; amarillo claro
    ;;'(web-mode-html-attr-name-face ((t (:foreground "lightblue")))) ; azul-claro
    ;;'(web-mode-html-attr-name-face ((t (:foreground "#BCEDDE")))) ;verde claro
-   
-   '(web-mode-html-attr-value-face ((t (:foreground "#D1907F")))) ; naranja fuerte
-   ;;'(web-mode-html-attr-value-face ((t (:foreground "#CAB4CC")))) ;
-   ;;'(web-mode-html-attr-value-face ((t (:foreground "#FFDAB5")))) ;
-   '(web-mode-html-attr-value-face ((t (:foreground "#deaece")))) ; rosa fuerte
-   '(web-mode-html-attr-value-face ((t (:foreground "#e3bcd6")))) ; rosa claro
+
+   '(web-mode-html-attr-value-face ((t (:foreground "#edb0a0")))) ; naranja claro
+   ;;'(web-mode-html-attr-value-face ((t (:foreground "#D1907F")))) ; naranja fuerte
+   ;;'(web-mode-html-attr-value-face ((t (:foreground "#deaece")))) ; rosa fuerte
+   ;;'(web-mode-html-attr-value-face ((t (:foreground "#e3bcd6")))) ; rosa claro
 
    ;;'(web-mode-comment-face ((t (:foreground "#EFFBFF")))) ;; Color blanco
-   
+
    ;;----------------------JS2-mode------------------------------------
    '(js2-error ((t (:foreground "#ed5f70" :slant italic :weight semi-bold))))
    '(js2-warning ((t (:underline (:color "#EB9490" :style wave) :slant italic))))
-   '(js2-object-property ((t (:foreground "#FFDAB5")))) ; naranja claro
-   ;;'(js2-function-call ((t (:foreground "#FFDAB5")))) ; 
-   ;;'(js2-external-variable ((t (:foreground "#F4B3A0")))) ;
-   '(js2-function-param ((t (:foreground "#d6d6d6")))) ;; color de la letra normal 
+   ;;'(js2-object-property ((t (:foreground "#FFDAB5")))) ; naranja claro
+   '(js2-object-property ((t (:foreground "#d6d6d6" :slant italic)))) ; letra normal
+   '(js2-function-call ((t (:foreground "#fff4cc")))) ; amarillo
+   '(js2-external-variable ((t (:foreground "#FFDAB5")))) ; naranja claro
+   '(js2-function-param ((t (:foreground "#d6d6d6")))) ;; color de la letra normal
 
+   ;;--------------------diredfl------------------------------
+   '(diredfl-date-time ((t (:foreground "#b8c4d7"))))
+   '(diredfl-dir-heading ((t (:foreground "#ceeca4"))))
+   '(diredfl-dir-name ((t (:foreground "#F0DFAF"))))
+   '(diredfl-dir-priv ((t (:foreground "#96e7e5"))))
+   '(diredfl-exec-priv ((t (:foreground "#D1907F"))))
+   '(diredfl-file-name ((t nil)))
+   '(diredfl-file-suffix ((t (:inherit diredfl-file-name))))
+   '(diredfl-no-priv ((t nil)))
+   '(diredfl-number ((t (:foreground "#F0DFAF"))))
+   '(diredfl-read-priv ((t (:foreground "#ceeca4"))))
+   '(diredfl-symlink ((t (:foreground "#96e7e5"))))
+   '(diredfl-write-priv ((t (:foreground "#F0DFAF"))))
 
    )
-)
+  )
 
 
 ;;;###Autoload
